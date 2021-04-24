675983364c273ada55e87638c42a1d94486815a2
punnammani <83112903+punnammani@users.noreply.github.com>
2021-04-24 16:11:41 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -3,7 +3,7 @@ def call()
 emailext attachLog: true, attachmentsPattern: 'Release_Status.csv', body: '''Hi Team,
 ***This is auto generated mail from local machine Jenkins !!!***
 Job_Name :: Pipeline-CSVReader
-Username :: Ullas Govindappa
+Username :: Punnam Durga Manikanta
 #Please find the attached result [Release_Status.csv]. Kindly check and confirm
 Regards,
 Mani
